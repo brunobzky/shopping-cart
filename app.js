308afa4da7f2cb4efe27b602cce3ac7e861dd94f
bjauregui2 <brunoalonso.jauregui@dxc.com>
2020-07-26 15:17:27 -0500

    Cierre de sesión
    Se añade cierre de sesión,  ligas para los botones de la barra de navegación,  se agrupan todos los gets y post relacionados con user.

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
 
+var indexRouter = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 
 var app = express();
@@ -40,8 +40,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
